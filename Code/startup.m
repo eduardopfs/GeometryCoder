cc4790a5756de393b4defc6f8685bf5e364e472d
José Edil Guimarães De Medeiros <j.edil@ene.unb.br>
2019-11-04 16:50:11 -0300

    Paths now work for Unix and Windows.

--- Code/startup.m
+++ Code/startup.m
@@ -1,40 +1,50 @@
 %Putting the source folder in the matlab path.
-
 clc
 
 version = 'SPL';
 disp(['Initializing Point Cloud Geometry coder version ' version ''])
 
+% Check if this is a Window$ or a Unix machine because paths
+% work diffferently
+comp = computer();
+if isequal(comp, 'PCWIN64')
+    bar = '\';
+else
+    bar = '/';
+end
+
+% Build folder to add to path.
 home = pwd;
-src = [home '\' version];
+src = [home bar version];
 disp(['Adding ' src ' to the Matlab Path'])
 addpath(src);
 
-src = [home '\' version '\ArithmeticCoding'];
+src = [home bar version bar 'ArithmeticCoding'];
 disp(['Adding ' src ' to the Matlab Path'])
 addpath(src);
 
-src = [home '\' version '\Structs'];
+src = [home bar version bar 'Structs'];
 disp(['Adding ' src ' to the Matlab Path'])
 addpath(src);
 
-src = [home '\' version '\Bitstream'];
+src = [home bar version bar 'Bitstream'];
 disp(['Adding ' src ' to the Matlab Path'])
 addpath(src);
 
-src = [home '\' version '\Encoder'];
+src = [home bar version bar 'Encoder'];
 disp(['Adding ' src ' to the Matlab Path'])
 addpath(src);
 
-src = [home '\' version '\Decoder'];
+src = [home bar version bar 'Decoder'];
 disp(['Adding ' src ' to the Matlab Path'])
 addpath(src);
 
-src = [home '\' version '\Utils'];
+src = [home bar version bar 'Utils'];
 disp(['Adding ' src ' to the Matlab Path'])
 addpath(src);
 
 clear home;
 clear src;
 
+% Move workspace to where code is.
 cd(version);
