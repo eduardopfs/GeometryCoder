8a7dad3140eb3b54876031cca8b7a0395f8a4f8a
José Edil Guimarães De Medeiros <j.edil@ene.unb.br>
2019-11-13 18:31:47 -0300

    Extreme simplification on get2DContext function

--- Code/SPL/ArithmeticCoding/get2DContext.m
+++ Code/SPL/ArithmeticCoding/get2DContext.m
@@ -8,53 +8,17 @@
 % E-mail: eduardopeixoto@ieee.org
 function contextNumber = get2DContext(A, pixel, numberOfContexts)
 
+% Initialize constants across repeated calls.
+contX = [-1 0 +1 -1 -2 0 -2 +2 -1 +1 -2 +2 -3 0 -3 +3];
+contY = [0 -1 -1 -1 0 -2 -1 -1 -2 -2 -2 -2 0 -3 -1 -1];
+
 y = pixel(1) + 3;
 x = pixel(2) + 3;
 
-if (numberOfContexts == 5)
-    contextNumber = double(A(y    , x - 1));
-    contextNumber = contextNumber +  2 * A(y - 1, x    );
-    contextNumber = contextNumber +  4 * A(y - 1, x + 1);
-    contextNumber = contextNumber +  8 * A(y - 1, x - 1);
-    contextNumber = contextNumber + 16 * A(y    , x - 2);    
-elseif (numberOfContexts == 10)    
-    contextNumber = double(A(y    , x - 1));
-    contextNumber = contextNumber +   2 * A(y - 1, x    );
-    contextNumber = contextNumber +   4 * A(y - 1, x + 1);
-    contextNumber = contextNumber +   8 * A(y - 1, x - 1);
-    contextNumber = contextNumber +  16 * A(y    , x - 2);
-    contextNumber = contextNumber +  32 * A(y - 2, x    );
-    contextNumber = contextNumber +  64 * A(y - 1, x - 2);
-    contextNumber = contextNumber + 128 * A(y - 1, x + 2);
-    contextNumber = contextNumber + 256 * A(y - 2, x - 1);
-    contextNumber = contextNumber + 512 * A(y - 2, x + 1);
-else
-    %Initialize all contexts as zeros.
-    %c = zeros(1,16);
-    c = [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
-    
-    %All contexts exist! Just go get them.
-    c(1)  = A(y    , x - 1);
-    c(2)  = A(y - 1, x    );
-    c(3)  = A(y - 1, x + 1);
-    c(4)  = A(y - 1, x - 1);
-    c(5)  = A(y    , x - 2);
-    c(6)  = A(y - 2, x    );
-    c(7)  = A(y - 1, x - 2);
-    c(8)  = A(y - 1, x + 2);
-    c(9)  = A(y - 2, x - 1);
-    c(10) = A(y - 2, x + 1);
-    c(11) = A(y - 2, x - 2);
-    c(12) = A(y - 2, x + 2);
-    c(13) = A(y    , x - 3);
-    c(14) = A(y - 3, x    );
-    c(15) = A(y - 1, x - 3);
-    c(16) = A(y - 1, x + 3);
-    
-    c = double(c);
-    p2 = [1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768];
-    p2c = p2 .* c;
-    
-    contextNumber = sum(p2c(1:numberOfContexts));
-end
+index = sub2ind(...
+    size(A), ...
+    y + contY(1:numberOfContexts), ...
+    x + contX(1:numberOfContexts));
+
+contextNumber = sum(2.^(0:numberOfContexts-1) .* A(index(1:numberOfContexts)));
     
